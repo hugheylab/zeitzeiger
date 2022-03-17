08d3679fa63255f911357dff6973d5278349fb01
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2022-03-17 15:50:55 -0500

    True fix for single quote

--- .github/workflows/custom-lint-script.R
+++ .github/workflows/custom-lint-script.R
@@ -52,8 +52,6 @@ lfDt[, format_line :=
               lint_link, ')', newlineEsc, '```', newlineEsc, line, newlineEsc, '```')]
 issueStr = paste0(lfDt$format_line, collapse = newlineEsc)
 
-issueStr = gsub("'", "%27", issueStr)
+issueStr = gsub("'", "'\"'\"'", issueStr, fixed = TRUE)
 
 s = sprintf("echo '::set-output name=style_text::%s'", issueStr)
-
-system(s)
